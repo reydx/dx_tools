727e5a93c6daa379c0db4857901dfc3a7239cc9c
czp <caozhengping@dxchain.onaliyun.com>
2020-06-08 17:16:05 +0800

    config

---
+++ scripts/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+"""
+@Summary : 
+@Author  : Rey
+@Time    : 2020-06-08 17:10
+@Log     :
+           author datetime(DESC) summary
+"""
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+"""
+@Summary : 
+@Author  : Rey
+@Time    : 2020-06-08 17:09
+@Log     :
+           author datetime(DESC) summary
+"""
+
+from setuptools import find_packages, setup
+setup(
+    name='dx_tools',
+    version='1.0.0',
+    description='tools',
+    author='Rey',
+    author_email='czp_fist@163.com',
+    # url='https://github.com/szmgithubcode/',
+    url='https://github.com/reydx/dx_tools',
+    #packages=find_packages(),
+    packages=['scripts'],
+    #install_requires=['requests'],
+)
