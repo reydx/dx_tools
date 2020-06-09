26d80cfc972fd01b78e4ac723a13b4d6ba5716e2
czp <caozhengping@dxchain.onaliyun.com>
2020-06-09 13:59:41 +0800

    rey:修改setup

--- setup.py
+++ setup.py
@@ -7,15 +7,23 @@
            author datetime(DESC) summary
 """
 
+import io
 from setuptools import find_packages, setup
+
+VERSION = "1.0.3"
+
+with io.open("README.md", 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='dx_tools',
-    version='1.0.2',
+    version=VERSION,
     description='tools',
     author='Rey',
     author_email='czp_fist@163.com',
-    # url='https://github.com/szmgithubcode/',
     url='https://github.com/reydx/dx_tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     #packages=find_packages(),
     packages=['dx_tools'],
     #install_requires=['requests'],
